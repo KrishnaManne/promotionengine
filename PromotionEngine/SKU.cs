2d2a7efe3a003d723fd9287453295247158d227e
Krishna Manne <krishna.manne9@live.com>
2020-06-16 11:19:27 +0530

    Added SKU types values dictionary
    Added SKU types and their values.

---
+++ PromotionEngine/SKU.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public static class SKU
+    {
+        public static Dictionary<char, int> SKUTypes = new Dictionary<char, int>
+        {
+            {'A', 50 },
+            {'B', 30 },
+            {'C', 20 },
+            {'D', 15 }
+        };
+    }
+}
