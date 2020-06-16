6f3ebb8027bf02d0ec7baea5d73132ddec2e8a76
Krishna Manne <krishna.manne9@live.com>
2020-06-16 12:02:57 +0530

    Refactoring - Implemented ThreeAsPromotion
    Added a new ThreeAsPromotion strategy for implementing 3 A's promotion algorithm.

---
+++ PromotionEngine/ThreeAsPromotion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class ThreeAsPromotion : PromotionStrategyAbstractBase
+    {
+        public override int Calculate(Dictionary<char, int> orderList)
+        {
+            return GroupOfSameSKUsPromotion(orderList, 'A', 3, 130);
+        }
+    }
+}
