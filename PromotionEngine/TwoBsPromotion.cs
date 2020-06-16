e92ea9da89a07439196494618c3e706a89d1d391
Krishna Manne <krishna.manne9@live.com>
2020-06-16 12:05:23 +0530

    Refactoring - Implemented TwoBsPromotion
    Added new TwoBsPromotion strategy class for implementing Two B's promotion algorithm.

---
+++ PromotionEngine/TwoBsPromotion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class TwoBsPromotion : PromotionStrategyAbstractBase
+    {
+        public override int Calculate(Dictionary<char, int> orderList)
+        {
+            return GroupOfSameSKUsPromotion(orderList, 'B', 2, 45);
+        }
+    }
+}
