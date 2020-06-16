5361ebff9474f568c4cff5e4d980bb910d6a2b32
Krishna Manne <krishna.manne9@live.com>
2020-06-16 13:15:13 +0530

    Renaming argument for better name

--- PromotionEngine/PromotionStrategyAbstractBase.cs
+++ PromotionEngine/PromotionStrategyAbstractBase.cs
@@ -7,14 +7,14 @@ namespace PromotionEngine
     {
         public abstract int Calculate(Dictionary<char, int> orderList);
 
-        protected int GroupOfSameSKUsPromotion(Dictionary<char, int> orderList, char key, int groupSize, int offerValue)
+        protected int GroupOfSameSKUsPromotion(Dictionary<char, int> orderList, char skuType, int groupSize, int offerValue)
         {
             if (orderList == null || orderList.Count == 0)
                 return 0;
 
-            var itemCount = orderList.FirstOrDefault(x => x.Key == key).Value;
+            var itemCount = orderList.FirstOrDefault(x => x.Key == skuType).Value;
             int newItemCount = itemCount % groupSize;
-            orderList[key] = newItemCount;
+            orderList[skuType] = newItemCount;
             return (itemCount / groupSize) * offerValue;
         }
     }
