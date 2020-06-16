5152564b95e858baf2968ac146298305899e2fa1
Krishna Manne <krishna.manne9@live.com>
2020-06-16 12:40:48 +0530

    Refactoring OrderValueCalculator

--- PromotionEngine/OrderValueCalculator.cs
+++ PromotionEngine/OrderValueCalculator.cs
@@ -6,6 +6,12 @@ namespace PromotionEngine
 {    
     public class OrderValueCalculator : IOrderValueCalculator
     {
+        private IPromotionStrategyContext promotionsContext;
+
+        public OrderValueCalculator()
+        {
+            promotionsContext = new PromotionStrategyContext();
+        }
         public int Calculate(List<char> orderList)
         {
             int result = 0;
@@ -14,37 +20,9 @@ namespace PromotionEngine
 
             var aggList = orderList.GroupBy(x => x).ToDictionary(k => k.Key, k => k.Count());
 
-            // 3 of A's for 130
-            var aCount = aggList.FirstOrDefault(x => x.Key == 'A').Value;
-            int newCountOfA = aCount % 3;
-            result = (aCount / 3) * 130;
-            aggList['A'] = newCountOfA;
-
-            // 2 of B's for 45
-            var bCount = aggList.FirstOrDefault(x => x.Key == 'B').Value;
-            result += (bCount / 2) * 45;
-            int newCountOfB = bCount % 2;
-            aggList['B'] = newCountOfB;
-
-            // C & D for 30
-            int cCount = aggList.FirstOrDefault(y => y.Key == 'C').Value;
-            int dCount = aggList.FirstOrDefault(y => y.Key == 'D').Value;
-            int offer = cCount > 0 && dCount > 0 ? cCount & dCount : 0;
-            int diff = cCount ^ dCount;
-            result += offer * 30;
-            if (cCount > dCount)
+            foreach(var promotion in promotionsContext.Promotions)
             {
-                aggList['C'] = diff;
-                aggList['D'] = 0;
-            }
-            else if(dCount > cCount)
-            {
-                aggList['D'] = diff;
-                aggList['C'] = 0;
-            }
-            else
-            {   
-                aggList['C'] = aggList['D'] = 0;
+                result += promotion.Calculate(aggList);
             }
 
             // Calculate individual SKU values
