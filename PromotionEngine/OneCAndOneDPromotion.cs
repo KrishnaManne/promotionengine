541b733da19a65d49420ad85bbaf4170823ff986
Krishna Manne <krishna.manne9@live.com>
2020-06-16 12:29:05 +0530

    Refactoring - Added the logic for OneCAndOneDPromotion
    Added the logic for OneCAndOneDPromotion and satisfies the tests.

--- PromotionEngine/OneCAndOneDPromotion.cs
+++ PromotionEngine/OneCAndOneDPromotion.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace PromotionEngine
 {
@@ -6,7 +7,30 @@ namespace PromotionEngine
     {
         public override int Calculate(Dictionary<char, int> orderList)
         {
-            throw new System.NotImplementedException();
+            if (orderList == null || orderList.Count() == 0)
+                return 0;
+
+            int cCount = orderList.FirstOrDefault(y => y.Key == 'C').Value;
+            int dCount = orderList.FirstOrDefault(y => y.Key == 'D').Value;
+            int offer = cCount > 0 && dCount > 0 ? cCount & dCount : 0;
+            int diff = cCount ^ dCount;
+
+            if (cCount > dCount)
+            {
+                orderList['C'] = diff;
+                orderList['D'] = 0;
+            }
+            else if (dCount > cCount)
+            {
+                orderList['D'] = diff;
+                orderList['C'] = 0;
+            }
+            else
+            {
+                orderList['C'] = orderList['D'] = 0;
+            }
+
+            return offer * 30;
         }
     }
 }
