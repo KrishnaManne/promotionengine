6a5431c2dcd7568b8d28e46fb8d807a1043ca6ae
Krishna Manne <krishna.manne9@live.com>
2020-06-16 12:37:26 +0530

    Refactoring - Added PromotionStrategyContext
    Added PromotionStrategyContext interface and implementation.

---
+++ PromotionEngine/IPromotionStrategyContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public interface IPromotionStrategyContext
+    {
+        List<IPromotionStrategy> Promotions { get; }
+
+    }
+}
---
+++ PromotionEngine/PromotionStrategyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class PromotionStrategyContext : IPromotionStrategyContext
+    {
+        public PromotionStrategyContext()
+        {
+            Promotions = new List<IPromotionStrategy>();
+            Promotions.Add(new ThreeAsPromotion());
+            Promotions.Add(new TwoBsPromotion());
+            Promotions.Add(new OneCAndOneDPromotion());
+        }
+
+        public List<IPromotionStrategy> Promotions { get; private set; }
+    }
+}
