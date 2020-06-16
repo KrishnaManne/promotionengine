147f8d6e10b0ccdbde086f4ab29122305b4ef039
Krishna Manne <krishna.manne9@live.com>
2020-06-16 12:27:49 +0530

    Refactoring - Added tests for OneCAndOneDPromotion
    Added new promotion and add the test cases first.

---
+++ PromotionEngine.Tests/OneCAndOneDPromotionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace PromotionEngine.Tests
+{
+    public class OneCAndOneDPromotionTests
+    {
+        [Fact]
+        public void GivenMultipleCAndOneDThenPrmotionShouldBeApplied()
+        {
+            IPromotionStrategy strategy = new OneCAndOneDPromotion();
+            var result = strategy.Calculate(new Dictionary<char, int> { { 'C', 3 },{'D' , 1 } });
+            Assert.Equal(30, result);
+        }
+
+        [Fact]
+        public void GivenTwoCAndTwoDThenPrmotionShouldBeAppliedTwoTimes()
+        {
+            IPromotionStrategy strategy = new OneCAndOneDPromotion();
+            var result = strategy.Calculate(new Dictionary<char, int> { { 'C', 2 }, { 'D', 2 } });
+            Assert.Equal(60, result);
+        }
+
+        [Fact]
+        public void GivenEmptyListThenTwoBsPrmotionShouldReturnZero()
+        {
+            IPromotionStrategy strategy = new OneCAndOneDPromotion();
+            var result = strategy.Calculate(new Dictionary<char, int>());
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ PromotionEngine/OneCAndOneDPromotion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine
+{
+    public class OneCAndOneDPromotion : PromotionStrategyAbstractBase
+    {
+        public override int Calculate(Dictionary<char, int> orderList)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
