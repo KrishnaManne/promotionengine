33d16b5bf9b3865c5682a50f744eddaef8aba659
Krishna Manne <krishna.manne9@live.com>
2020-06-16 11:06:13 +0530

    InitialCommit
    Create a new .net standard project and named it as promotion engine.
    Added the OrderValueCalculator class.

---
+++ PromotionEngine/OrderValueCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromotionEngine
+{
+    public class OrderValueCalculator
+    {
+        public int Calculate(List<char> orderList)
+        {
+            return -1;
+        }
+    }
+}
